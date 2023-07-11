615f92de7df4b8d841b1910af2def1a95c0bdb1f
vivekrudra980 <vivekprudra@gmail.com>
2023-07-11 05:03:54 +0000

    exporting an object

---
+++ exportObject.js
@@ -0,0 +1,10 @@
+// Create two files exportObject.js, importObject.js parallel to README.md file.
+
+// Write an object with keys as firstName and lastName and values as John and Wilson respectively in the file exportObject.js and export it using the default export syntax.
+
+// Import the object in the importObject.js file.
+
+// Use Common JS module syntax.
+
+let object = { firstName: "John", lastName: "Wilson" };
+module.exports = object;
---
+++ importObject.js
@@ -0,0 +1,2 @@
+let object = require("./exportObject");
+console.log(object);
